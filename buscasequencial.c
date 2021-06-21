2ad076b1d86f2198199310afa6a0034cb0d503c1
Hiago Carlos <hiagocarlosmoreira@hotmail.com>
2021-06-21 00:01:19 -0300

    busca sequencial

---
+++ buscasequencial.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 5
+
+int buscasequencial(int *vetor, int k)
+{
+    for (int i = 0; i < TAM; i++)
+    {
+        if (vetor[i] == k)
+        {
+            return vetor[i];
+        }
+    }
+
+    return -1;
+}
+
+int sentinela(int *vetor, int k)
+{
+    int i;
+
+    vetor[TAM] = k;
+    for (i = 0; i < vetor[i] != k; i++)
+    {
+    }
+
+    if (i < TAM)
+    {
+        return vetor[i];
+    }
+
+    return -1;
+}
+
+
+int movendoElemeto(int *vetor, int k){
+    int aux;
+    
+    for (int i = 0; i < TAM; i++)
+    {
+        if(vetor[i] == k)
+        {
+            aux = vetor[i];
+            for (int j = 0; j <= i; j++)
+            {
+                vetor[j+1] = vetor[j];
+            }
+            vetor[0] = aux;
+            return aux;            
+        }
+    }
+
+    return -1;
+    
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+    int vet[TAM], chave, retorno;
+
+    for (int i = 0; i < TAM; i++)
+    {
+        printf("\n Informe um numero : ");
+        scanf("%i", &vet[i]);
+    }
+
+    printf("\n Informe um a chave : ");
+    scanf("%i", &chave);
+
+    retorno = sentinela(vet, chave);
+
+    if (retorno != -1)
+    {
+        printf("\n encontoru a chave");
+    }
+    else
+    {
+        printf("\n chave nao encontrada");
+    }
+
+    return 0;
+}
